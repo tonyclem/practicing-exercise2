e0cf192853e4a03c9cebc2437f520da5eccfdce6
tonyclem <tonyclem001@gmail.com>
2022-02-05 22:41:48 +0100

    modified the index1 and 2, and move it to perp-exercise, still working on the perp-exercise

---
+++ prep-exercise/index1.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// The state property says what the trafic light's state
+const trafficLight = {
+  state: "green",
+};
+
+let rotations = 0;
+while (rotations < 2) {
+  const currentState = trafficLight.state;
+  if (!currentState) {
+    console.log("the traffic light is on", currentState);
+  } else {
+    console.log("The traffic light is on yellow");
+  }
+  break;
+}
