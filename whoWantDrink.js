cd3d63e1e5121e36a59c955dff662d69d685b07c
tonyclem <tonyclem001@gmail.com>
2022-02-10 08:35:55 +0100

    added comment to the code.

--- whoWantDrink.js
+++ whoWantDrink.js
@@ -9,25 +9,33 @@
 
 // There are 3 different types of drinks:
 const drinkTypes = ["cola", "lemonade", "water"];
+
+// i declared this variable in other to use it with each property in the drinkTypes
 let cola = 0;
 let lemonade = 0;
 let water = 0;
 
+// here we hava an empty array called drinkTray that will receive value
 let drinkTray = [];
-while (drinkTray.length < 5) {
-  let drinkBar = drinkTypes[Math.floor(Math.random() * drinkTypes.length)];
 
-  if (drinkBar === "cola" && cola < 2) {
+// using while loop, to loop throught the drinkTray length, to check if the length is less then 5, as long as the test condition evaluates to true. the condition will continues looping until is falsy
+while (drinkTray.length < 5) {
+  // declaring new variable called drinkFromBar and passing drinkTypes to, as a new value, and using Math random to random the drinkTypes
+  let drinkFromBar = drinkTypes[Math.floor(Math.random() * drinkTypes.length)];
+
+  // using if conditional statement to check if drinkFromBar have the property call cola with  === operator, as well as using and && logical and passing cole, water and lemonade, but by default i set it to zero. checking if the boolean property i pass in is less the 2, then increment it by one and push the value of drinkFromBar inside the empty array called drinkTray .
+  if (drinkFromBar === "cola" && cola < 2) {
     cola++;
-    drinkTray.push(drinkBar);
-  } else if (drinkBar === "water" && water < 2) {
+    drinkTray.push(drinkFromBar);
+  } else if (drinkFromBar === "water" && water < 2) {
     water++;
-    drinkTray.push(drinkBar);
-  } else if (drinkBar === "lemonade" && lemonade < 2) {
+    drinkTray.push(drinkFromBar);
+  } else if (drinkFromBar === "lemonade" && lemonade < 2) {
     lemonade++;
-    drinkTray.push(drinkBar);
+    drinkTray.push(drinkFromBar);
   }
 }
-console.log(`"Hey guys, I brought a ${drinkTray}"`);
+// this print out our values
+console.log(`Hey guys, I brought some drink a ${drinkTray}`);
 
 // OutPrint Buy: "Hey guys, I brought a water,lemonade,cola,lemonade,cola"
